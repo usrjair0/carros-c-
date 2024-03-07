822d0c43d8877b1ab7138e1b1f7b1a8f725397b2
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-03-07 09:33:02 -0300

    carro02 class added some changes

--- Carro02/Program.cs
+++ Carro02/Program.cs
@@ -10,10 +10,11 @@ namespace Carro02
     {
         public double potencia;
         public int numeroDePortas;
-        public string cor;
-        public string nome;
-        public int velocidade;
-        public void acelerar()
+        private string cor;
+        private string nome;
+        private int velocidade;  //caso eu deixe como private eu não consigo acessar os meus métodos fora do escopo da classe
+
+        public void acelerar() //método ou função, mas é mais chamado de método
         {
             this.velocidade++; //o this faz referência a struct Carro
         }
@@ -21,6 +22,41 @@ namespace Carro02
         {
             this.velocidade--;
         }
+
+        public int getVelocidade() 
+        { 
+            return this.velocidade;
+        }
+
+        public void setNome(string nome) //string nome é parâmetro
+        {
+            //return early
+            if (nome.Length > 100)
+                throw new ArgumentException("numero de caracteres de nome foi excedido - 100 é o máximo");    
+            
+            this.nome = nome;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public string Cor //é uma convenção deixar como maiúsculo
+        {
+            //Essa forma é exclusiva do framework
+            get 
+            { 
+                return this.cor; 
+            }
+            set 
+            { 
+                this.cor = value; 
+            }
+        }
+
+
+
 
         public Carro() //Aqui estou criando uma instancia toda vez que faço um new ele passa por aqui.
             //deve ter o mesmo nome da class
@@ -42,9 +78,9 @@ namespace Carro02
             Carro ferrari = new Carro(); //new é uma palavra reservada do c# para invocar o método construtor
             ferrari.potencia = 10; 
 
-            Carro carro1 = new Carro();
+            //Carro carro1 = new Carro();
             Carro carro2 = new Carro();
-            Carro carro3 = new Carro();
+            //Carro carro3 = new Carro();
 
             List < Carro > carros = new List<Carro>();
 
@@ -54,9 +90,19 @@ namespace Carro02
                 carros.Add(carro);
             }
 
-            ferrari.nome = "Ferrari do zequinha";
+            ferrari.setNome("Ferrari do zequinha");
 
-            carros[2].nome = "fusca";
+            carros[2].setNome("fusca");
+            carros[1].setNome(ferrari.getNome());
+            carros[2] = carro2;
+            carros[2].setNome("chevete");
+            carro2.setNome("celtinha chiqueirinho");
+
+            carro2.acelerar();
+            carro2.getVelocidade();
+
+            ferrari.Cor = "branca"; // como estou atribuindo ele vai passar no método set
+            Console.WriteLine($"A cor da ferrari é {ferrari.Cor}");
         }
     }
 }
