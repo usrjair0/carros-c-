0ef8179708101c2e334ea6df1c0e5d2e3638cf67
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-03-20 11:13:05 -0300

    add consultorio

---
+++ consultorio/Consulta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consultorio
+{
+    internal class Consulta
+    {
+        public int Codigo { get;}
+        public DateTime DataHora { get; set; }
+        public Medico Medico { get; set; }
+
+        public Paciente Paciente { get; set; }
+
+
+        public Consulta (int codigo)
+        {
+            Codigo = codigo;
+        }
+    }
+}
